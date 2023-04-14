41c4700c2a1573019b37fabad0e4c2b1d5208358
James-Randall-14 <jammerand14@gmail.com>
2023-04-13 21:37:44 -0700

    yet another joyswitch version

--- src/imageCapture.py
+++ src/imageCapture.py
@@ -1,42 +1,40 @@
+#!/usr/bin/env python3
+
 import cv2
 import rospy
-from img_capture.msg import controlData, camData
+from sensor_msgs.msg import Image
+from img_capture.msg import controlData
 import os
 
 # Global Variables
-path = "/home/JHSrobo/ROVMIND/ros_workspace/src/img_capture/img"
+path = "/home/jhsrobo/ROVMIND/ros_workspace/src/img_capture/img"
 count = 1
 coralCount = 1
-ip = None
 coralMode = False
-videoFeed = None
 
-# Subscribers
-camera_sub = rospy.Subscriber('cameras', camData, cam_callback)
-control_sub = rospy.Subscriber('control', control_data, control_callback)
-
-def cam_callback(camData):
-    global path, count, coralCount, ip, coralMode, videoFeed
-    if camData.screenshot:
-        if ip != camData.ip:
-            videoFeed.release()
-            videoFeed = cv2.VideoCapture('http://{}:5000'.format(ip))
-        image = videoFeed.read()
+def img_callback(screenshot):
+    global path, count, coralCount, coralMode
         if coralMode:
-            cv2.imwrite("{}/coral/{}.png".format(path, coralCount), image)
-            coralCount++
+            cv2.imwrite("{}/coral/{}.png".format(path, coralCount), screenshot)
+            coralCount += 1
         else:
-            cv2.imwrite("{}/{}.png".format(path, count), image)
-            count++
+            cv2.imwrite("{}/{}.png".format(path, count), screenshot)
+            count += 1
 
 def control_callback(controlData):
     global coralMode
     coralMode = controlData.coral
 
+# Subscribers
+camera_sub = rospy.Subscriber('screenshots', Image, img_callback)
+control_sub = rospy.Subscriber('control', controlData, control_callback)
+
 def main():
     rospy.init_node('camera_feed')
-    os.system("rm /home/jhsrobo/ROVMIND/ros_workspace/src/img_capture/imgs/*.png")
-    os.system("rm /home/jhsrobo/ROVMIND/ros_workspace/src/img_capture/imgs/coral/*.png")
+    try: os.system('rm {}/*.png'.format(path))
+    except: pass
+    try: os.system('rm {}/coral/*.png'.format(path))
+    except: pass
 
     rospy.spin()
     cv2.destroyAllWindows()
